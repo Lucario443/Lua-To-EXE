45d63e250e41514bac31cc25b3cf6f4095872e5d
Lucario443 <Lucario@vivaldi.net>
2019-07-21 14:10:11 -0400

    Update Convert.cmd

--- Convert.cmd
+++ Convert.cmd
@@ -2,7 +2,7 @@
 @echo off
 echo Converting...
 glue.exe srlua.exe input.lua output.exe
-echo Finished.
+echo Finished. You may close this window now.
 echo Press any key to re-convert . . .
 pause > nul
 goto q
