122a1f33b220010d599282c7b9164e8c8bca8ab9
ExtremePro11299 <134209046+ExtremePro11299@users.noreply.github.com>
2023-06-23 18:50:21 +0200

    Change "convert" to "compile"

--- Convert.cmd
+++ Convert.cmd
@@ -1,9 +1,9 @@
 @echo off
 :q
-echo Converting...
+echo Compiling...
 glue.exe srlua.exe input.lua output.exe
 echo Finished. What do you want to do?
-echo 1) Convert again
+echo 1) Compile again
 echo 2) Exit
 set /p opt=
 if %opt%==1 goto q
